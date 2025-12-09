6787e8ac7df70e1bbfb36b414c21223c0f72896b
yarbework <yarbemohaz3517@gmail.com>
2025-12-09 23:55:19 +0300

    creating http server

---
+++ task1/http-server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+if(req.method === 'GET'){
+        if (req.url === '/'){
+            res.end('Welcome to the Home Page')
+        }
+        else if(req.url==='/info'){
+            res.end('This is the information page')
+        }
+        else{
+            res.end('Page not Found');
+        }
+}
+if(req.method === 'POST'){
+    if(req.url === '/submit'){
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            res.end(`Data received: ${body}`);
+        });
+    }
+    else{
+        res.end('Page not Found');
+    }
+}
+})
+
+server.listen(3000, ()=> {
+    console.log('Server is running')
+})
